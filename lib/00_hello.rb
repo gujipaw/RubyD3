2af74c25ebfd248345f189d34143b756eca5db38
Faty60 <sidib.fatou2@gmail.com>
2021-04-14 18:05:34 +0200

    2nd commit

--- lib/00_hello.rb
+++ lib/00_hello.rb
@@ -1,6 +1,19 @@
-puts "Donne-moi ton nom"
-def say_hello
+def ask_first_name
+    puts "Donne-moi ton prenom"
+    print "> "
     first_name = gets.chomp
-    puts "Bonjour, #{first_name} !"
+    return first_name
 end
-puts say_hello
+
+def greet(first_name)
+     puts "Bienvenue, #{first_name} !" 
+end
+
+def perform
+    first_name = ask_first_name
+    greet (first_name)
+end
+
+perform
+
+
